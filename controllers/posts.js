c593e4795d1b7e7d5b9d2eb55d0717b690beb408
André de Mattos Ferraz <fandre@gmail.com>
2015-04-15 17:44:56 -0300

    crud post
    crud post

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,26 +8,65 @@ module.exports = function(app) {
 			console.log(JSON.stringify(post));
 
 			Post.create(post, function(erro, post){
+				res.setHeader('Content-Type', 'application/json');
 				if(erro){
-					res.setHeader('Content-Type', 'application/json');
 					res.end(JSON.stringify({ success : false, message:"Xiiiii deu zica!" + erro }));
 					return;
 				}
 
-				res.setHeader('Content-Type', 'application/json');
-				res.end(JSON.stringify({ success : true, message:"Post criado!" }));
+				res.end(JSON.stringify({ success : true, message:"Post criado!", data: post }));
 			});
 		},
 		getPosts: function(req, res){
 			Post.find({}, function(erro, posts) {
+				res.setHeader('Content-Type', 'application/json');
 				if(erro){
-					res.setHeader('Content-Type', 'application/json');
 					res.end(JSON.stringify({ success : false, message:"Xiiiii deu zica!" + erro }));
 					return;
 				}
 
-				res.setHeader('Content-Type', 'application/json');
 				res.end(JSON.stringify({ success : true, message:"", data:posts }));
+			});
+		},
+		deletePost: function(req, res){
+			var id = req.body.id;
+			//console.log(JSON.stringify(req.body));
+			console.log(JSON.stringify(id));
+
+			Post.remove({ _id: id }, function(erro) {
+				res.setHeader('Content-Type', 'application/json');
+				if (!erro) {
+					res.end(JSON.stringify({ success : false, message:"Post deletado!", data: id}));
+				}
+				else {
+					res.end(JSON.stringify({ success : false, message:"Xiiiii deu zica!" + erro }));
+				}
+			});
+		},
+		editPost: function(req, res){
+			var post = req.body.post;
+			console.log(JSON.stringify(post));
+			Post.findByIdAndUpdate(id, { $set: post }, function (erro, post) {
+				res.setHeader('Content-Type', 'application/json');
+				if (!erro) {
+					res.end(JSON.stringify({ success : false, message:"Post editado!", data : post}));
+				}
+				else {
+					res.end(JSON.stringify({ success : false, message:"Xiiiii deu zica!" + erro }));
+				}
+			});
+		},
+		getPost: function(req, res){
+			var id = req.body.id;
+			console.log(JSON.stringify(id));
+			Post.findById(id, function (erro, post) {
+				res.setHeader('Content-Type', 'application/json');
+				if (!erro) {
+					res.end(JSON.stringify({ success : false, message:"Post encontrado!", data : post}));
+				}
+				else {
+					res.end(JSON.stringify({ success : false, message:"Xiiiii deu zica!" + erro }));
+				}
 			});
 		}
 	};
--- routes/post.js
+++ routes/post.js
@@ -2,4 +2,7 @@ module.exports = function(app) {
 	var post = app.controllers.posts;
 	app.post('/post/createPost', post.createPost);
 	app.post('/post/getPosts', post.getPosts);
+	app.post('/post/deletePost', post.deletePost);
+	app.post('/post/editPost', post.editPost);
+	app.post('/post/getPost', post.getPost);
 };
